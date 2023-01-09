ed067b3941befc4c67a36fd2cb1f21f444566d8f
roddev0 <118647241+roddev0@users.noreply.github.com>
2023-01-09 09:39:36 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
-const caracteres ="ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz12345678$@#ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz12345678$@#"
+const caracteres ="ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz12345678$@#"
 let array = caracteres.split('')
-let largoo = array.length
+
 
 function print(){
 let password=""
 let x = document.getElementById("input").value;
 for (i = 0; i < x; i++) {    
-    let num = Math.random() * array.length
+    let num = Math.random() * array.length - 1
     num = parseInt(num.toFixed(0))    
     let letra =  array[num]
     password += letra    

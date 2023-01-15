1232f7e4ffd0df006ffdd236d4d5207db3cb9e67
roddev0 <118647241+roddev0@users.noreply.github.com>
2023-01-15 12:14:49 -0300

    Update index.js

--- index.js
+++ index.js
@@ -6,8 +6,13 @@ function print(){
 let password=""
 let x = document.getElementById("input").value;
 for (i = 0; i < x; i++) {    
-    let num = Math.floor(Math.random() * array.length + 1)    
-    let letra =  array[num]
-    password += letra    
+    let num = Math.floor(Math.random() * array.length + 1)
+    if (num > -1 && num < 63){
+        let letra =  array[num]
+        password += letra    
+    }else{
+        let num = Math.floor(Math.random() * array.length + 1)
+    }   
+
     }document.getElementById("txt").innerHTML = password
 }
